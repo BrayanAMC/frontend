f98cd4736c8be4888cff2fdc678bd05c0f3656b0
BRAYAN ALEXIS MALDONADO CARRASCO <brayan.maldonado@alumnos.ucn.cl>
2024-03-19 17:21:07 -0300

    feat: listo para modo dev y produccion

--- components/apolloConfig/apolloConfig.ts
+++ components/apolloConfig/apolloConfig.ts
@@ -1,5 +1,5 @@
 import { createHttpLink } from "@apollo/client";
 
 export const httpLink = createHttpLink({
-  uri: `https://${process.env.NEXT_PUBLIC_GATEWAY_DOMAIN}/graphql` || 'http://localhost:3002/graphql',
+  uri: process.env.NEXT_PUBLIC_GATEWAY_DOMAIN ? `https://${process.env.NEXT_PUBLIC_GATEWAY_DOMAIN}/graphql` : 'http://localhost:3002/graphql',
 });
